bc5c31a8ee1c7948087517745f596240abf8a270
alberto <alberto.jauregui@fujitsu.com>
2021-07-13 07:49:28 +0200

    install cors and solve problems with them

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var consumptions = require("./routes/consumptions");
-require("./mongooseConnect");
 
 var app = express();
 
 // conectamos la base de datos
+require("./mongooseConnect");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/consumptions", consumptions);
 
