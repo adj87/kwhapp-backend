1b825aae79dee95f73ca261e238c000fb43e55bb
albertojauregui <albertodomjauregui@hotmail.com>
2021-07-14 22:23:09 +0200

    change url_db_connection

--- mongooseConnect.js
+++ mongooseConnect.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 const conn = mongoose.connection;
-
+const db_url = "mongodb+srv://user:KJdd815nCXpOyiNP@cluster0.3kckz.mongodb.net/jalajala";
 conn.on("error", (err) => {
   console.log("Error de conexión", err);
   process.exit(1);
@@ -16,7 +16,7 @@ conn.once("close", () => {
   console.log("Connection closed", mongoose.connection.name);
 });
 
-mongoose.connect("mongodb://localhost/prueba-fs", {
+mongoose.connect(db_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
